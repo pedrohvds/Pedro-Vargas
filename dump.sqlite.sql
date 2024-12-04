80bd3fc1768f7ea721c852c142aafc060bc9953c
pedrohvds <pedro2006vdesouza@gmail.com>
2024-12-04 16:27:51 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE tbclientes (
+	idclientes INT PRIMARY_KEY,
+  	nome VARCHAR(100),
+  	telefone VARCHAR(100),
+  	email VARCHAR(255)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
