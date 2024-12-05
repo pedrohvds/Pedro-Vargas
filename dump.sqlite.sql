46faca50ae0f0df441eb084f74b8d88b3ca6e772
pedrohvds <pedro2006vdesouza@gmail.com>
2024-12-05 16:36:24 -0300

    Add files via upload

--- dump.sqlite.sql
+++ dump.sqlite.sql
@@ -1,10 +1,17 @@
 -- TABLE
 CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Pedidos (
+  idPedido INT AUTO_INCREMENT PRIMARY KEY,
+  idCliente INT, 
+  numeroPedido varchar(50),
+  valorTotal decimal (10, 2),
+  dataPedido date,
+  FOREIGN KEY (idCliente) REFERENCES Clientes(idCliente)
+);
 CREATE TABLE tbclientes (
-	idclientes INT PRIMARY_KEY,
-  	nome VARCHAR(100),
-  	telefone VARCHAR(100),
-  	email VARCHAR(255)
+  idClientes INT PRIMARY KEY,
+  nome VARCHAR(100),
+  email VARCHAR(255)
 );
  
 -- INDEX
